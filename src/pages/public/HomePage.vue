724768b0925d938232e40c7b2ad0980aadefd021
Roy Duenas <rduenas@ingenuiti.com>
2024-05-23 19:04:20 +0800

    Desktop version

--- src/pages/public/HomePage.vue
+++ src/pages/public/HomePage.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        Home page
+    <div class="home-banner flex items-center justify-between" style="gap: 2rem;">
+        <div style="width: 50%;">
+            <div class="text-h3 text-public-primary text-bold">CPSU GRUADUATE SCHOOL</div>
+            <div class="text-h4 text-dark text-bold">NEWS AND UPDATES</div>
+            <div class="text-caption-1 q-my-lg">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum distinctio quia doloremque consequatur odio placeat consectetur quidem animi repellat eos. Officia, amet? Laborum, enim? Libero dolor natus aliquid perspiciatis tempora!
+            </div>
+        </div>
+        <div style="flex-grow: 1; height: inherit;" class="flex items-end justify-center">
+            <div>
+                <img src="~assets/home/person.png"/>
+            </div>
+        </div>
     </div>
 </template>
 
